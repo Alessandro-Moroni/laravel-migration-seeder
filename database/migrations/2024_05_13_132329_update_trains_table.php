db68d7a9de30311c8b3d41e29a6adc390dc88011
Alessandro-Moroni <alemoro98@gmail.com>
2024-05-13 15:36:55 +0200

    [did]exercise

---
+++ database/migrations/2024_05_13_132329_update_trains_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->string('seat_number',5)->after('number_carriages');
+            $table->boolean('delay')->default(false)->after('in_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('seat_number');
+            $table->dropColumn('delay');
+        });
+    }
+};
